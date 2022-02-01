abc666fd97e1eda9c85fc991bfd9ae107197f0dd
slanzarin <97183934+slanzarin@users.noreply.github.com>
2022-02-01 11:34:35 -0300

    Update index.php

--- views/site/index.php
+++ views/site/index.php
@@ -123,8 +123,8 @@ $this->title = 'My Yii Application';
         </div>
         <div class="col">
           <div class="card shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
+            <img class="card-img-top" src="img/ventre.png" alt="Card image cap" width="100%" height="100%">
+              
             <div class="card-body">
               <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
               <div class="d-flex justify-content-between align-items-center">
