74d4dfc900babd9526c8eaca7bd9d95d28b29065
slanzarin <97183934+slanzarin@users.noreply.github.com>
2022-02-14 14:07:44 -0300

    Create ventre.php

---
+++ web/ventre.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="en">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="img/ventre1.jpeg"  alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="img/ventre2.jpeg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="img/ventre3.jpeg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+<div>
+<h1>Conheça alguns professores:</h1>
+</div>
+
+<div class="card-group">
+  <div class="card">
+    <img class="card-img-top" src="img/prof1.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Jhonatas Andrade</h5>
+      <p class="card-text">Professor a 5 anos, se apresentou em diversos festivais e foi premiado no Festival Norte Dança em 2015</p>
+      <button type="button" class="btn btn-outline-primary "><a href="index.php?r=site%2Flogin">Conheça mais sobre</button></a>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="img/prof2.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Nathasha Ribeiro</h5>
+      <p class="card-text">Professora da Escola de Danças de Maranhão a 9 anos.</p>
+      <button type="button" class="btn btn-outline-primary "><a href="index.php?r=site%2Flogin">Conheça mais sobre</button></a>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="img/prof3.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Luiza Clara Santos </h5>
+      <p class="card-text">Premiada no Dança São Paulo em 1° lugar, começou a profisão de professora a 2 anos. </p>
+      <button type="button" class="btn btn-outline-primary "><a href="index.php?r=site%2Flogin">Conheça mais sobre</button></a> 
+    </div>
+  </div>
+</div>
+<div class="row py-lg-5">
+      <div class="col-lg-6 col-md-8 mx-auto" >
+        <p>
+       
+        <p class="lead text-muted">Conheça mais professores cadastrados no Dance Space.</p>
+        <p>
+          
+          <a href="index.php?r=site%2Fabout" class="btn btn-dark btn-lg">Saiba Mais </a>
+        </p>
+      </div>
+    </div>
+...
+</html>
